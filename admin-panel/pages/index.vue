22f0640b5f7e5997b03aa73299f4e91acff9c01b
phoebe <62113347+myifu@users.noreply.github.com>
2025-10-23 15:31:53 +0900

    [CNX] REMOVE: Unused meta tags and request URL logic from index.vue

--- admin-panel/pages/index.vue
+++ admin-panel/pages/index.vue
@@ -16,35 +16,6 @@
 definePageMeta({
   layout: 'concentrix',
 })
-
-// 현재 요청 URL 가져오기
-const requestUrl = useRequestURL()
-const baseUrl = `${requestUrl.protocol}//${requestUrl.host}`
-
-// 페이지별 메타데이터
-const pageTitle = 'Concentrix - A Global Technology and Service Leader'
-const pageDescription = ''
-const pageKeywords = ''
-const ogImage = `${baseUrl}/assets/concentrix-share.png`
-
-// 페이지별 메타 태그 (공통 메타데이터는 레이아웃에서 처리)
-useHead({
-  title: pageTitle,
-  meta: [
-    { name: 'description', content: pageDescription },
-    { name: 'keywords', content: pageKeywords },
-
-    // 페이지별 Open Graph 태그
-    { property: 'og:title', content: pageTitle },
-    { property: 'og:description', content: pageDescription },
-    { property: 'og:image', content: ogImage },
-
-    // 페이지별 Twitter Card 태그
-    { name: 'twitter:title', content: pageTitle },
-    { name: 'twitter:description', content: pageDescription },
-    { name: 'twitter:image', content: ogImage },
-  ],
-})
 </script>
 
 <style lang="scss" scoped>
